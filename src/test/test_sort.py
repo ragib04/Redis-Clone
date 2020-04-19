ad900430f746b6a6ce9f464be7dbc91a2d329560
tcd97 <mohammadazib581@gmail.com>
2020-04-20 02:27:58 +0530

    Initial Commit
    Files Upload

---
+++ src/test/test_sort.py
@@ -0,0 +1,9 @@
+from sortedcontainers import SortedSet
+s = SortedSet()
+s.add((1981, 'Brad'))
+s.add((1975, 'John'))
+s.add((1990, 'Mike'))
+s.add((1990, 'Kate'))
+s.add((1990, 'ABC'))
+
+print(type(s) == int)
